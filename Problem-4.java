d0f0c3cf9b75cbe7beeee9e8123a07fee3b9df40
Md. Shahrukh Khan <shahrukhkhan3418198@gmail.com>
2025-09-22 11:56:20 +0600

    Sep 22 java codechef

---
+++ Problem-4.java
@@ -0,0 +1,9 @@
+//Print Learn Coding on CodeChef
+//Print "Learn Coding on CodeChef" to the console.
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Learn Coding on CodeChef");
+    }
+}
---
+++ Problem-5.java
@@ -0,0 +1,8 @@
+// Print 108 using 9 and 12 Print the number 108 using arithmetic operations on the numbers 9 and 12.
+
+
+class Codechef {
+    public static void main(String[] args) {
+        System.out.println(9 * 12);
+    }
+}
