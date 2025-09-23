a7b77f1f3f8e32343e4d1619da9ba006120e85ad
Md. Shahrukh Khan <shahrukhkhan3418198@gmail.com>
2025-09-23 07:22:52 +0600

    Median of Two Sorted Arrays

---
+++ Problem-9.cpp
@@ -0,0 +1,67 @@
+// Median of Two Sorted Arrays
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2)
+    {
+        if (nums1.size() > nums2.size())
+            return findMedianSortedArrays(nums2, nums1);
+
+        int m = nums1.size();
+        int n = nums2.size();
+        int low = 0, high = m;
+
+        while (low <= high)
+        {
+            int partitionX = (low + high) / 2;
+            int partitionY = (m + n + 1) / 2 - partitionX;
+
+            int maxLeftX = (partitionX == 0) ? INT_MIN : nums1[partitionX - 1];
+            int minRightX = (partitionX == m) ? INT_MAX : nums1[partitionX];
+
+            int maxLeftY = (partitionY == 0) ? INT_MIN : nums2[partitionY - 1];
+            int minRightY = (partitionY == n) ? INT_MAX : nums2[partitionY];
+
+            if (maxLeftX <= minRightY && maxLeftY <= minRightX)
+            {
+                if ((m + n) % 2 == 0)
+                {
+                    return (double)(max(maxLeftX, maxLeftY) + min(minRightX, minRightY)) / 2;
+                }
+                else
+                {
+                    return (double)max(maxLeftX, maxLeftY);
+                }
+            }
+            else if (maxLeftX > minRightY)
+            {
+                high = partitionX - 1;
+            }
+            else
+            {
+                low = partitionX + 1;
+            }
+        }
+
+        throw invalid_argument("Input arrays are not sorted correctly.");
+    }
+};
+
+int main()
+{
+    Solution sol;
+    vector<int> nums1 = {1, 3};
+    vector<int> nums2 = {2};
+    cout << fixed << setprecision(5);
+    cout << sol.findMedianSortedArrays(nums1, nums2) << endl; // 2.00000
+
+    vector<int> nums3 = {1, 2};
+    vector<int> nums4 = {3, 4};
+    cout << sol.findMedianSortedArrays(nums3, nums4) << endl; // 2.50000
+
+    return 0;
+}
