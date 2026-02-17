b3e50101467f40bdbff9a304ddf1610ec4a29383
Md. Shahrukh Khan <shahrukhkhan3418198@gmail.com>
2026-02-17 06:37:38 +0600

    The word arguments cannot be used as a variable

--- Problem-54.js
+++ Problem-54.js
@@ -2,5 +2,12 @@
 
 "use strict";
 
-let value = 3;
-console.log(value);
+// let value = 3;
+// console.log(value);
+
+// The word arguments cannot be used as a variable:
+
+function test(a, b) {
+  console.log(arguments)
+}
+test(1, 3)
