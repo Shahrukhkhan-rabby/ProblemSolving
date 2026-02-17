ce7d9f6ed0081a9da8234fdfb484461d615c7b28
Md. Shahrukh Khan <shahrukhkhan3418198@gmail.com>
2026-02-17 06:58:23 +0600

    The with statement is not allowed

--- Problem-54.js
+++ Problem-54.js
@@ -7,7 +7,15 @@
 
 // The word arguments cannot be used as a variable:
 
-function test(a, b) {
-  console.log(arguments)
-}
-test(1, 3)
+// function test(a, b) {
+//   console.log(arguments)
+// }
+// test(1, 3)
+
+// The with statement is not allowed:
+
+// let x = Math.cos(2);
+// console.log(object)
+const {cos} = Math;
+let x = cos(23);
+console.log(x)
